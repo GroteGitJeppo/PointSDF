5b62ec162ca88e7ea5f59a32b3300c97f820e451
GroteGitJeppo <j.m.spuesens@tilburguniversity.edu>
2026-03-12 14:56:56 +0100

    Implementation of the PointNet++ encoder by reusing all the code from the PointNet2_Pytorch repository

--- data/dataset_sdf.py
+++ data/dataset_sdf.py
@@ -6,34 +6,90 @@ import results
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+
 class SDFDataset(Dataset):
     """
-    TODO: adapting to handle multiple objects
+    Dataset for the encoder-decoder SDF architecture.
+
+    Each sample returns a triplet:
+        - pointcloud : (num_points, 3) float tensor  - encoder input for this object
+        - coords     : (3,)           float tensor  - 3D query point
+        - sdf        : (1,)           float tensor  - ground-truth SDF value
+
+    Memory layout: point clouds are stored ONCE per object (not tiled per SDF sample).
+    A sample-to-object index mapping is used in __getitem__ to look up the correct
+    point cloud without duplicating data.
     """
+
     def __init__(self, dataset_name, results_folder=None):
         if results_folder is None:
             results_folder = os.path.dirname(results.__file__)
-        samples_dict = np.load(os.path.join(results_folder, f'samples_dict_{dataset_name}.npy'), allow_pickle=True).item()
-        self.data = dict()
-        for obj_idx in list(samples_dict.keys()):  # samples_dict.keys() for all the objects
-            for key in samples_dict[obj_idx].keys():   # keys are ['samples', 'sdf', 'latent_class', 'samples_latent_class']
-                value = torch.from_numpy(samples_dict[obj_idx][key]).float().to(device)
-                if len(value.shape) == 1:    # increase dim if monodimensional, needed to vstack
-                    value = value.view(-1, 1)
-                if key not in list(self.data.keys()):
-                    self.data[key] = value
-                else:
-                    self.data[key] = torch.vstack((self.data[key], value))
-        return
+        samples_dict = np.load(
+            os.path.join(results_folder, "samples_dict_" + dataset_name + ".npy"),
+            allow_pickle=True,
+        ).item()
+
+        sdf_list = []
+        coords_list = []
+        sample_to_obj_list = []   # maps each SDF sample to its object index
+        pc_per_obj_list = []      # one point cloud per object
+
+        for i, obj_key in enumerate(sorted(samples_dict.keys())):
+            obj_data = samples_dict[obj_key]
+
+            # samples_latent_class: (N, 4) -> columns are [latent_class, x, y, z]
+            samples_latent_class = obj_data["samples_latent_class"]  # (N, 4)
+            sdf = obj_data["sdf"]                                     # (N,)
+            pointcloud = obj_data["pointcloud"]                       # (num_points, 3)
+
+            num_samples = samples_latent_class.shape[0]
+
+            sdf_list.append(sdf.reshape(-1, 1).astype(np.float32))
+            coords_list.append(samples_latent_class[:, 1:].astype(np.float32))  # (N, 3)
+
+            # Record which object index each sample belongs to
+            sample_to_obj_list.append(np.full(num_samples, i, dtype=np.int64))
+
+            # Store this object's point cloud once
+            pc_per_obj_list.append(pointcloud.astype(np.float32))  # (num_points, 3)
+
+        self.sdf = torch.from_numpy(np.vstack(sdf_list)).float().to(device)
+        self.coords = torch.from_numpy(np.vstack(coords_list)).float().to(device)
+
+        # sample_to_obj: (total_samples,) - integer index into pointclouds_per_obj
+        self.sample_to_obj = torch.from_numpy(
+            np.concatenate(sample_to_obj_list)
+        ).long().to(device)
+
+        # pointclouds_per_obj: (num_objects, num_points, 3) - stored only once per object
+        self.pointclouds_per_obj = torch.from_numpy(
+            np.stack(pc_per_obj_list)
+        ).float().to(device)
+
+        # Keep a data dict so the trainer can apply SDF clamping before training starts
+        self.data = {
+            "sdf": self.sdf,
+            "coords": self.coords,
+        }
 
     def __len__(self):
-        return self.data['sdf'].shape[0]
+        return self.sdf.shape[0]
 
     def __getitem__(self, idx):
-        latent_class = self.data['samples_latent_class'][idx, :]
-        sdf = self.data['sdf'][idx]
-        return latent_class, sdf
+        obj_idx = self.sample_to_obj[idx]
+        return (
+            self.pointclouds_per_obj[obj_idx],   # (num_points, 3)
+            self.coords[idx],                     # (3,)
+            self.sdf[idx],                        # (1,)
+        )
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     dataset_name = "Potato"
-    dataset = SDFDataset(dataset_name)
+    ds = SDFDataset(dataset_name)
+    pc, coords, sdf = ds[0]
+    print("Point cloud shape: " + str(pc.shape))
+    print("Coords shape:      " + str(coords.shape))
+    print("SDF shape:         " + str(sdf.shape))
+    print("Num objects:       " + str(ds.pointclouds_per_obj.shape[0]))
+    print("Total samples:     " + str(len(ds)))
--- model/train_sdf.py
+++ model/train_sdf.py
@@ -1,10 +1,10 @@
 import torch
 import model.model_sdf as sdf_model
+import model.encoder_pointnet2 as encoder_module
 import torch.optim as optim
 import data.dataset_sdf as dataset
 from torch.utils.data import random_split
 from torch.utils.data import DataLoader
-import results.runs_sdf as runs
 from utils.utils_deepsdf import SDFLoss_multishape
 import os
 import random
@@ -13,14 +13,14 @@ from datetime import datetime
 import numpy as np
 import time
 from tqdm import tqdm
-from utils import utils_deepsdf
 import results
 from torch.utils.tensorboard import SummaryWriter
 import yaml
 import config_files
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-print(f'Device: {device}')
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(f"Device: {device}")
+
 
 class Trainer():
     def __init__(self, train_cfg, resultsfolder):
@@ -28,68 +28,65 @@ class Trainer():
         self.resultsfolder = resultsfolder
 
     def __call__(self):
-        self.timestamp_run = datetime.now().strftime('%d_%m_%H%M%S')
-        self.runs_dir = os.path.join(self.resultsfolder, 'runs_sdf')
+        self.timestamp_run = datetime.now().strftime("%d_%m_%H%M%S")
+        self.runs_dir = os.path.join(self.resultsfolder, "runs_sdf")
         self.run_dir = os.path.join(self.runs_dir, self.timestamp_run)
         if not os.path.exists(self.run_dir):
             os.makedirs(self.run_dir)
-        
+
         # Logging
         self.writer = SummaryWriter(log_dir=self.run_dir)
-        self.log_path = os.path.join(self.run_dir, 'settings.yaml')
-        with open(self.log_path, 'w') as f:
+        self.log_path = os.path.join(self.run_dir, "settings.yaml")
+        with open(self.log_path, "w") as f:
             yaml.dump(self.train_cfg, f)
 
-        # calculate num objects in samples_dictionary, wich is the number of keys
-        samples_dict_path = os.path.join(self.resultsfolder, f'samples_dict_{self.train_cfg["dataset"]}.npy')
-        samples_dict = np.load(samples_dict_path, allow_pickle=True).item()
+        # Instantiate encoder (PointNet2) and decoder (DeepSDF)
+        self.encoder = encoder_module.PointNet2Encoder(
+            latent_size=self.train_cfg["latent_size"],
+            dropout=self.train_cfg.get("encoder_dropout", 0.3),
+        ).float().to(device)
 
-        # instantiate model and optimisers
         self.model = sdf_model.SDFModel(
-                self.train_cfg['num_layers'], 
-                self.train_cfg['skip_connections'], 
-                inner_dim=self.train_cfg['inner_dim'],
-                latent_size=self.train_cfg['latent_size']
-            ).float().to(device)
+            self.train_cfg["num_layers"],
+            self.train_cfg["skip_connections"],
+            inner_dim=self.train_cfg["inner_dim"],
+            latent_size=self.train_cfg["latent_size"],
+        ).float().to(device)
 
-        # define optimisers
-        self.optimizer_model = optim.Adam(self.model.parameters(), lr=self.train_cfg['lr_model'], weight_decay=0)
-        
-        # generate a unique random latent code for each shape
-        self.latent_codes = utils_deepsdf.generate_latent_codes(self.train_cfg['latent_size'], samples_dict)
-        self.optimizer_latent = optim.Adam([self.latent_codes], lr=self.train_cfg['lr_latent'], weight_decay=0)
-        
-        # Load pretrained weights and optimisers to continue training
-        if self.train_cfg['pretrained']:
-            # load pretrained weights
-            self.model.load_state_dict(torch.load(self.train_cfg['pretrain_weights'], map_location=device))
+        # Single optimiser for both encoder and decoder
+        all_params = list(self.encoder.parameters()) + list(self.model.parameters())
+        self.optimizer = optim.Adam(
+            all_params,
+            lr=self.train_cfg["lr_model"],
+            weight_decay=0,
+        )
 
-            # load pretrained optimisers
-            self.optimizer_model.load_state_dict(torch.load(self.train_cfg['pretrain_optim_model'], map_location=device))
+        # Load pretrained weights to continue training
+        if self.train_cfg["pretrained"]:
+            checkpoint = torch.load(self.train_cfg["pretrain_weights"], map_location=device)
+            self.encoder.load_state_dict(checkpoint["encoder"])
+            self.model.load_state_dict(checkpoint["decoder"])
+            self.optimizer.load_state_dict(
+                torch.load(self.train_cfg["pretrain_optimizer"], map_location=device)
+            )
 
-            # retrieve latent codes from results.npy file
-            results_path = self.train_cfg['pretrain_optim_model'].split(os.sep)
-            results_path[-1] = 'results.npy'
-            results_path = os.sep.join(results_path)
-            # load latent codes from results.npy file
-            results_latent_codes = np.load(results_path, allow_pickle=True).item()
-            self.latent_codes = torch.tensor(results_latent_codes['best_latent_codes']).float().to(device)
-            self.optimizer_latent = optim.Adam([self.latent_codes], lr=self.train_cfg['lr_latent'], weight_decay=0)
-            self.optimizer_latent.load_state_dict(torch.load(self.train_cfg['pretrain_optim_latent'], map_location=device))
+        if self.train_cfg["lr_scheduler"]:
+            self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                self.optimizer,
+                mode="min",
+                factor=self.train_cfg["lr_multiplier"],
+                patience=self.train_cfg["patience"],
+                threshold=0.0001,
+                threshold_mode="rel",
+            )
 
-        if self.train_cfg['lr_scheduler']:
-            self.scheduler_model =  torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer_model, mode='min', factor=self.train_cfg['lr_multiplier'], patience=self.train_cfg['patience'], threshold=0.0001, threshold_mode='rel')
-            self.scheduler_latent =  torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer_latent, mode='min', factor=self.train_cfg['lr_multiplier'], patience=self.train_cfg['patience'], threshold=0.0001, threshold_mode='rel')
-            
-        # get data
+        # Get data loaders
         train_loader, val_loader = self.get_loaders()
-        self.results = {
-            'best_latent_codes' : []
-        }
 
-        best_loss = 10000000000
+        best_loss = 1e10
         start = time.time()
-        for epoch in tqdm(range(self.train_cfg['epochs']), desc="Epochs", unit="epoch"):
+
+        for epoch in tqdm(range(self.train_cfg["epochs"]), desc="Epochs", unit="epoch"):
             self.epoch = epoch
 
             avg_train_loss = self.train(train_loader)
@@ -98,100 +95,123 @@ class Trainer():
                 avg_val_loss = self.validate(val_loader)
 
                 if avg_val_loss < best_loss:
-                    best_loss = np.copy(avg_val_loss)
-                    best_weights = self.model.state_dict()
-                    best_latent_codes = self.latent_codes.detach().cpu().numpy()
-                    optimizer_model_state = self.optimizer_model.state_dict()
-                    optimizer_latent_state = self.optimizer_latent.state_dict()
+                    best_loss = float(avg_val_loss)
+                    torch.save(
+                        {
+                            "encoder": self.encoder.state_dict(),
+                            "decoder": self.model.state_dict(),
+                        },
+                        os.path.join(self.run_dir, "weights.pt"),
+                    )
+                    torch.save(
+                        self.optimizer.state_dict(),
+                        os.path.join(self.run_dir, "optimizer_state.pt"),
+                    )
 
-                    np.save(os.path.join(self.run_dir, 'results.npy'), self.results)
-                    torch.save(best_weights, os.path.join(self.run_dir, 'weights.pt'))
-                    torch.save(optimizer_model_state, os.path.join(self.run_dir, 'optimizer_model_state.pt'))
-                    torch.save(optimizer_latent_state, os.path.join(self.run_dir, 'optimizer_latent_state.pt'))
-                    self.results['best_latent_codes'] = best_latent_codes
+                if self.train_cfg["lr_scheduler"]:
+                    self.scheduler.step(avg_val_loss)
+                    self.writer.add_scalar(
+                        "Learning rate", self.scheduler._last_lr[0], epoch
+                    )
 
-                if self.train_cfg['lr_scheduler']:
-                    self.scheduler_model.step(avg_val_loss)
-                    self.scheduler_latent.step(avg_val_loss)
-
-                    self.writer.add_scalar('Learning rate (model)', self.scheduler_model._last_lr[0], epoch)
-                    self.writer.add_scalar('Learning rate (latent)', self.scheduler_latent._last_lr[0], epoch)            
-            
         end = time.time()
-        print(f'Time elapsed: {end - start} s')
+        print(f"Time elapsed: {end - start:.1f} s")
 
     def get_loaders(self):
-        data = dataset.SDFDataset(self.train_cfg['dataset'], results_folder=self.resultsfolder)
+        data = dataset.SDFDataset(
+            self.train_cfg["dataset"], results_folder=self.resultsfolder
+        )
 
-        if self.train_cfg['clamp']:
-            data.data['sdf'] = torch.clamp(data.data['sdf'], -self.train_cfg['clamp_value'], self.train_cfg['clamp_value'])
+        if self.train_cfg["clamp"]:
+            data.data["sdf"] = torch.clamp(
+                data.data["sdf"],
+                -self.train_cfg["clamp_value"],
+                self.train_cfg["clamp_value"],
+            )
+            # Sync the attribute used in __getitem__
+            data.sdf = data.data["sdf"]
 
         train_size = int(0.85 * len(data))
         val_size = len(data) - train_size
         train_data, val_data = random_split(data, [train_size, val_size])
+
         train_loader = DataLoader(
-                train_data,
-                batch_size=self.train_cfg['batch_size'],
-                shuffle=True,
-                drop_last=True
-            )
+            train_data,
+            batch_size=self.train_cfg["batch_size"],
+            shuffle=True,
+            drop_last=True,
+        )
         val_loader = DataLoader(
             val_data,
-            batch_size=self.train_cfg['batch_size'],
+            batch_size=self.train_cfg["batch_size"],
             shuffle=False,
-            drop_last=True
-            )
+            drop_last=True,
+        )
         return train_loader, val_loader
 
     def generate_xy(self, batch):
         """
-        Combine latent code and coordinates.
-        Return:
-            - x: latent codes + coordinates, torch tensor shape (batch_size, latent_size + 3)
-            - y: ground truth sdf, shape (batch_size, 1)
-            - latent_codes_indices_batch: all latent class indices per sample, shape (batch size, 1).
-                                            e.g. [[2], [2], [1], ..] eaning the batch contains the 2nd, 2nd, 1st latent code
-            - latent_batch_codes: all latent codes per sample, shape (batch_size, latent_size)
-        Return ground truth as y, and the latent codes for this batch.
+        Encode point clouds and combine latent vectors with 3D coordinates.
+
+        Args:
+            batch: tuple of (pointcloud, coords, sdf)
+                - pointcloud : (B, num_points, 3)
+                - coords     : (B, 3)
+                - sdf        : (B, 1)
+
+        Returns:
+            x       : (B, latent_size + 3) encoder output concatenated with coords
+            y       : (B, 1) ground-truth SDF
+            latent  : (B, latent_size) predicted latent vector (for regularisation)
         """
-        latent_classes_batch = batch[0][:, 0].view(-1, 1).to(torch.long)               # shape (batch_size, 1)
-        coords = batch[0][:, 1:]                                  # shape (batch_size, 3)
-        latent_codes_batch = self.latent_codes[latent_classes_batch.view(-1)]    # shape (batch_size, 128)
+        pointcloud, coords, sdf = batch
+        pointcloud = pointcloud.to(device)
+        coords = coords.to(device)
+        sdf = sdf.to(device)
 
-        x = torch.hstack((latent_codes_batch, coords))                  # shape (batch_size, 131)
-        y = batch[1]     # (batch_size, 1)
+        latent = self.encoder(pointcloud)              # (B, latent_size)
+        x = torch.hstack((latent, coords))             # (B, latent_size + 3)
+        y = sdf                                        # (B, 1)
 
-        return x, y, latent_classes_batch.view(-1), latent_codes_batch
+        return x, y, latent
 
     def train(self, train_loader):
         total_loss = 0.0
         iterations = 0.0
         self.model.train()
-        for batch in tqdm(train_loader, desc=f"Train Epoch {self.epoch}", leave=False, unit="batch"):
-            # batch[0]: [class, x, y, z], shape: (batch_size, 4)
-            # batch[1]: [sdf], shape: (batch size)
+        self.encoder.train()
+
+        for batch in tqdm(
+            train_loader, desc=f"Train Epoch {self.epoch}", leave=False, unit="batch"
+        ):
             iterations += 1.0
+            self.optimizer.zero_grad()
 
-            self.optimizer_model.zero_grad()
-            self.optimizer_latent.zero_grad()
+            x, y, latent = self.generate_xy(batch)
 
-            x, y, latent_codes_indices_batch, latent_codes_batch = self.generate_xy(batch)
+            predictions = self.model(x)  # (B, 1)
+            if self.train_cfg["clamp"]:
+                predictions = torch.clamp(
+                    predictions,
+                    -self.train_cfg["clamp_value"],
+                    self.train_cfg["clamp_value"],
+                )
 
-            predictions = self.model(x)  # (batch_size, 1)
-            if self.train_cfg['clamp']:
-                predictions = torch.clamp(predictions, -self.train_cfg['clamp_value'], self.train_cfg['clamp_value'])
-            
-            loss_value, loss_rec, loss_latent = self.train_cfg['loss_multiplier'] * SDFLoss_multishape(y, predictions, x[:, :self.train_cfg['latent_size']], sigma=self.train_cfg['sigma_regulariser'])
-            loss_value.backward()       
+            loss_value, loss_rec, loss_latent = SDFLoss_multishape(
+                y,
+                predictions,
+                latent,
+                sigma=self.train_cfg["sigma_regulariser"],
+            )
+            loss_value = self.train_cfg["loss_multiplier"] * loss_value
+            loss_value.backward()
+            self.optimizer.step()
 
-            self.optimizer_latent.step()
-            self.optimizer_model.step()
-            total_loss += loss_value.data.cpu().numpy()  
+            total_loss += loss_value.data.cpu().numpy()
 
-        avg_train_loss = total_loss/iterations
-        print(f'Training: loss {avg_train_loss}')
-        self.writer.add_scalar('Training loss', avg_train_loss, self.epoch)
-
+        avg_train_loss = total_loss / iterations
+        print(f"Training: loss {avg_train_loss:.6f}")
+        self.writer.add_scalar("Training loss", avg_train_loss, self.epoch)
         return avg_train_loss
 
     def validate(self, val_loader):
@@ -200,49 +220,55 @@ class Trainer():
         total_loss_latent = 0.0
         iterations = 0.0
         self.model.eval()
+        self.encoder.eval()
 
         for batch in tqdm(val_loader, desc="Validation", leave=False, unit="batch"):
-            # batch[0]: [class, x, y, z], shape: (batch_size, 4)
-            # batch[1]: [sdf], shape: (batch size)
-            iterations += 1.0            
+            iterations += 1.0
 
-            x, y, _, latent_codes_batch = self.generate_xy(batch)
+            x, y, latent = self.generate_xy(batch)
 
-            predictions = self.model(x)  # (batch_size, 1)
-            if train_cfg['clamp']:
-                predictions = torch.clamp(predictions, -train_cfg['clamp_value'], train_cfg['clamp_value'])
+            predictions = self.model(x)  # (B, 1)
+            if self.train_cfg["clamp"]:
+                predictions = torch.clamp(
+                    predictions,
+                    -self.train_cfg["clamp_value"],
+                    self.train_cfg["clamp_value"],
+                )
 
-            loss_value, loss_rec, loss_latent = self.train_cfg['loss_multiplier'] * SDFLoss_multishape(y, predictions, latent_codes_batch, self.train_cfg['sigma_regulariser'])          
-            total_loss += loss_value.data.cpu().numpy()   
-            total_loss_rec += loss_rec.data.cpu().numpy() 
-            total_loss_latent += loss_latent.data.cpu().numpy()
+            loss_value, loss_rec, loss_latent_reg = SDFLoss_multishape(
+                y,
+                predictions,
+                latent,
+                self.train_cfg["sigma_regulariser"],
+            )
+            total_loss += loss_value.data.cpu().numpy()
+            total_loss_rec += loss_rec.data.cpu().numpy()
+            total_loss_latent += loss_latent_reg.data.cpu().numpy()
 
-        avg_val_loss = total_loss/iterations
-        avg_loss_rec = total_loss_rec/iterations
-        avg_loss_latent = total_loss_latent/iterations
-        print(f'Validation: loss {avg_val_loss}')
-        self.writer.add_scalar('Validation loss', avg_val_loss, self.epoch)
-        self.writer.add_scalar('Reconstruction loss', avg_loss_rec, self.epoch)
-        self.writer.add_scalar('Latent code loss', avg_loss_latent, self.epoch)
+        avg_val_loss = total_loss / iterations
+        avg_loss_rec = total_loss_rec / iterations
+        avg_loss_latent = total_loss_latent / iterations
+
+        print(f"Validation: loss {avg_val_loss:.6f}")
+        self.writer.add_scalar("Validation loss", avg_val_loss, self.epoch)
+        self.writer.add_scalar("Reconstruction loss", avg_loss_rec, self.epoch)
+        self.writer.add_scalar("Latent code loss", avg_loss_latent, self.epoch)
 
         return avg_val_loss
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     torch.manual_seed(133)
     random.seed(133)
     np.random.seed(133)
 
     current_file = Path(__file__).resolve()
     project_root = current_file.parent.parent
-    data_folder = os.path.join(project_root, "data")
-    splits_csv = os.path.join(data_folder, "splits.csv")
-    weightsfolder = os.path.join(project_root, "weights")
     resultsfolder = os.path.join(project_root, "results")
-    os.makedirs(weightsfolder, exist_ok=True)
     os.makedirs(resultsfolder, exist_ok=True)
 
     config_path = os.path.join(project_root, "config_files", "train_sdf.yaml")
-    with open(config_path, 'rb') as f:
+    with open(config_path, "rb") as f:
         train_cfg = yaml.load(f, Loader=yaml.FullLoader)
 
     trainer = Trainer(train_cfg, resultsfolder)
