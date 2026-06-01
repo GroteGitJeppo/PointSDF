3510d8103edae4c74fae4e23cec84e12e48e3e90
GroteGitJeppo <j.m.spuesens@tilburguniversity.edu>
2026-06-01 14:10:27 +0200

    move to gpu based convex hull

--- utils/sdf_helpers.py
+++ utils/sdf_helpers.py
@@ -91,16 +91,12 @@ def sdf2mesh(
             "Try lowering the SDF threshold or increasing grid resolution."
         )
 
-    # Move to CPU before handing to Open3D. The conda-forge open3d build ships
-    # without CUDA support (BUILD_CUDA_MODULE=OFF), so passing a CUDA tensor via
-    # DLPack would raise "Unsupported device CUDA:0".
-    keep_points_cpu = keep_points.cpu().contiguous()
-    o3d_t = o3c.Tensor.from_dlpack(torch.utils.dlpack.to_dlpack(keep_points_cpu))
-    pcd_cpu = o3d.t.geometry.PointCloud(o3d_t)
+    o3d_t = o3c.Tensor.from_dlpack(torch.utils.dlpack.to_dlpack(keep_points))
+    pcd_gpu = o3d.t.geometry.PointCloud(o3d_t)
 
     voxel_size = 0.0
-    hull = pcd_cpu.compute_convex_hull()
-    mesh = _clean_mesh(hull.to_legacy())
+    hull_gpu = pcd_gpu.compute_convex_hull()
+    mesh = _clean_mesh(hull_gpu.to_legacy())
 
     while not mesh.is_watertight():
         voxel_size += 0.01  # match corepp/utils.sdf2mesh_cuda
@@ -109,9 +105,9 @@ def sdf2mesh(
                 "Could not produce a watertight mesh after progressive "
                 "voxel downsampling."
             )
-        down_pcd = pcd_cpu.voxel_down_sample(voxel_size=voxel_size)
-        hull = down_pcd.compute_convex_hull()
-        mesh = _clean_mesh(hull.to_legacy())
+        down_pcd = pcd_gpu.voxel_down_sample(voxel_size=voxel_size)
+        hull_gpu = down_pcd.compute_convex_hull()
+        mesh = _clean_mesh(hull_gpu.to_legacy())
 
     return mesh
 
